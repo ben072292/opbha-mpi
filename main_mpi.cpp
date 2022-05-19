c7b3fd61a7ffebdbe6c3faf1f3b0083ec437b0fc
weicongchen <ben072292@gmail.com>
2022-05-18 23:59:56 -0400

    new

--- main_mpi.cpp
+++ main_mpi.cpp
@@ -5,6 +5,9 @@
 #include <vector>
 #include <mpi.h>
 #include <thread>
+#include <omp.h>
+#include <algorithm>
+#include <random>
 using namespace std;
 
 bool is_done(bool* checked_set, int size, int world_rank){
@@ -17,14 +20,12 @@ bool is_done(bool* checked_set, int size, int world_rank){
     return false;
 }
 
-int* select_n(bool* checked_set, int* table, int size, int select_size){
-    int* ret = new int[select_size]{0};
+void select_n(bool* checked_set, int* table, int size, int select_size, vector<int>& ret){
     int counter = 0;
     for(int i = 0; i < size; i++){
         if(!checked_set[table[i]]) ret[counter++] = table[i];
         if(counter == select_size) break;
     }
-    return ret;
 }
 
 int main(int argc, char* argv[]){
@@ -44,15 +45,15 @@ int main(int argc, char* argv[]){
     int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
 
-
     int pool_size = atoi(argv[1]);
     double prior_val = atof(argv[2]);
+    omp_set_num_threads(atoi(argv[3]));
     double* prior = new double[pool_size];
     for(int i = 0; i < pool_size; i++) prior[i] = prior_val;
     pool_stat* pool = new pool_stat(pool_size, prior);
     lattice_model* model;
 
-    double total_mpi_bcast_time = 0.0;
+    double run_time = 0.0;
 
     if(world_rank == 0) {
         model = new lattice_model(pool, true);
@@ -63,53 +64,41 @@ int main(int argc, char* argv[]){
         model->set_posterior_probability_map((double*)malloc(sizeof(double) * (1 << pool_size)));
     }
     MPI_Barrier(MPI_COMM_WORLD);
-    total_mpi_bcast_time -= MPI_Wtime();
+    run_time -= MPI_Wtime();
     MPI_Bcast(model->get_posterior_probability_map(), (1 << pool_size), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    int select_size = (1 << (pool_size-5)); // 1/64
+    int select_size = (1 << (pool_size-6)); // 1/64
     int* sophsticated_table;
     bool* checked_set = (bool*)malloc(sizeof(bool) * (1 << pool_size));
-    bool* global_checked_set;
-    int* select;
+    vector<int> select;
+    auto rng = default_random_engine {};
     int sub_select_size = select_size / world_size;
     int* sub_select = (int*)malloc(sizeof(int) * sub_select_size);
-
     bool done = false;
     double local_min = 2.0;
     double global_min = 2.0;
 
-    if (world_rank == 0) {
+    // initialize rank 0 variables
+    if(world_rank == 0){
+        select.resize(select_size, 0);
         sophsticated_table = model->sophisticated_selection();
     }
     while(!done){
     // for(int k = 0; k < 3; k++){
         if (world_rank == 0) {
-            select = select_n(checked_set, sophsticated_table, 1<<pool_size, select_size);
+            select_n(checked_set, sophsticated_table, 1<<pool_size, select_size, select);
+            std::shuffle(begin(select), end(select), rng); // shuffle for better load balance
         }
         // Scatter states to all processes
-        MPI_Scatter(select, sub_select_size, MPI_INT, sub_select, sub_select_size, MPI_INT, 0, MPI_COMM_WORLD);
+        // MPI_Scatterv will be supported in the future for varied dispatch size
+        MPI_Scatter(select.data(), sub_select_size, MPI_INT, sub_select, sub_select_size, MPI_INT, 0, MPI_COMM_WORLD);
         
-        // if (world_rank == 0){
-        //     cout << "0: ";
-        //     for(int i = 0; i < sub_select_size; i++){
-        //         cout << sub_select[i] << " ";
-        //     }
-        //     cout << endl << endl;
-        // }
-
-        // if (world_rank == 1){
-        //     cout << "1: ";
-        //     for(int i = 0; i < sub_select_size; i++){
-        //         cout << sub_select[i] << " ";
-        //     }
-        //     cout << endl << endl;
-        // }
-        
-        double val = 2.0, temp = 2.0;
+        // auto start = chrono::high_resolution_clock::now();
+        #pragma omp parallel for schedule(dynamic)
         for(int i = 0; i < sub_select_size; i++){
             int state = sub_select[i];
             if(checked_set[state]) continue;
-            val = model->get_up_set_mass(state);
+            double val = model->get_up_set_mass(state);
             if(val < 0.5){
                 int* up_set = model->get_up_set(state);
                 for(int j = 0; j < (1 << (pool_size-__builtin_popcount(state))); j++) checked_set[up_set[j]] = true;
@@ -120,24 +109,22 @@ int main(int argc, char* argv[]){
                 for(int j = 0; j < (1 << __builtin_popcount(state)); j++) checked_set[down_set[j]] = true;
                 delete[] down_set;
             }
-            temp = abs(val - 0.5);
+            double temp = abs(val - 0.5);
             if(temp < local_min){
                 local_min = temp;
             }
         }
-
+        // auto stop = chrono::high_resolution_clock::now();
+        // auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        // cout << "Time: " << duration.count() << endl;
+        
+        // Reduce all of the local checked sets into process 0
+        // in place reduce
         if(world_rank == 0){
-            if(!global_checked_set) free(global_checked_set);
-            global_checked_set = (bool*)malloc(sizeof(bool) * (1 << pool_size));
+            MPI_Reduce(MPI_IN_PLACE, checked_set, 1<<pool_size, MPI_CXX_BOOL, MPI_LOR, 0, MPI_COMM_WORLD);
         }
-        // // Reduce all of the local checked sets into the global checked set
-        MPI_Reduce(checked_set, global_checked_set, 1<<pool_size, MPI_CXX_BOOL, MPI_LOR, 0, MPI_COMM_WORLD);
-        
-        
-        if(world_rank == 0){
-            delete[] select;
-            free(checked_set);
-            checked_set = global_checked_set;
+        else{
+            MPI_Reduce(checked_set, checked_set, 1<<pool_size, MPI_CXX_BOOL, MPI_LOR, 0, MPI_COMM_WORLD);
         }
 
         // Reduce all of the local min into the global min
@@ -157,15 +144,19 @@ int main(int argc, char* argv[]){
         MPI_Bcast(&done, 1, MPI_CXX_BOOL, 0, MPI_COMM_WORLD);
     }
     MPI_Barrier(MPI_COMM_WORLD);
-    total_mpi_bcast_time += MPI_Wtime();
+    run_time += MPI_Wtime();
 
     if(world_rank == 0){
-        cout << "Min: " << global_min << endl << "Time: " << total_mpi_bcast_time << "s" << endl;
+        cout << "Min: " << global_min << endl << "Time: " << run_time << "s" << endl;
     }
 
     // final cleanup
-    if(world_rank == 0) delete[] sophsticated_table;
+    if(world_rank == 0){
+        delete[] sophsticated_table;
+        // free(select);
+    }
     else model->free_posterior_probability_map();
+    
     free(checked_set);
 
     // Finalize the MPI environment.
